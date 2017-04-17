e5c5361a0cfb24e0d01d2a44672682aff0652343
Jake Faulkner <jakefaulkn@gmail.com>
2017-04-17 18:50:49 +1200

    Stub movement actions

--- src/rules.ml
+++ src/rules.ml
@@ -10,20 +10,26 @@ type selector_result =
   | Substr of string array
   | Match of bool
 let empty_selector _ = Match false
-let empty_action _ = Success
+let empty_action _ _ = return Success
 let rx_selector rx p =
-  Substr(Pcre.exec ~rex: rx p
-         |> Pcre.get_substrings)
-
-  (* Substr (Pcre.get_substrings (Pcre.exec ~rex: rx p)) *)
-let movement_action folder p = Success
-let delete_action p = Success
+  try
+    Substr(Pcre.exec ~rex: rx p
+           |> Pcre.get_substrings)
+  with Not_found ->
+    Match false
+let file_movement_action folder p s =
+  match s p with
+  | Substr groups -> Lwt_io.printl ("Moving " ^ p ^ " to " ^ folder) >>= fun () -> (return Success)
+  | Match true -> Lwt_io.printl ("Moving " ^ p ^ " to " ^ folder) >>= fun () -> (return Success)
+  | _ -> Lwt_io.printl ("Not performing any action on" ^ p) >>= fun () -> (return Success)
+let sweep_movement_action p s = Lwt_io.printl ("Sweeping " ^ folder) >>= fun () -> (return Success)
+let delete_action p s = return Success
 
 module Rule = struct
   type t = {
     path : string ;
     selector : (string -> selector_result);
-    action : (string -> action_result);
+    action : (string -> (string -> selector_result) -> action_result Lwt.t);
     watch : bool;
     poll : int option;
   }
@@ -36,9 +42,10 @@ module Rule = struct
   }
   let rec rule_of_ini path base_rule = function
     | ("selector", Regexp rx)::xs ->  rule_of_ini path {base_rule with selector = (rx_selector rx)} xs
-    | ("move_to", String folder)::xs -> rule_of_ini path {base_rule with action = (movement_action folder)} xs
+    | ("move_to", String folder)::xs -> rule_of_ini path {base_rule with action = sweep_movement_action} xs
     | ("delete", Bool true)::xs -> rule_of_ini path {base_rule with action = delete_action} xs
-    | ("watch", Bool true)::xs -> rule_of_ini path {base_rule with watch = true} xs
+    | ("watch", Bool true)::xs -> rule_of_ini path {base_rule with watch = true;
+                                                                   action = (file_movement_action path)} xs
     | ("poll", Time poll)::xs -> rule_of_ini path {base_rule with poll = Some poll} xs
     | kv::xs -> Result.Error kv
     | [] -> Result.Ok {base_rule with path = path}
